390253ce73aa64166e4ca93b867f711aab87dada
God Emperor <tarakindrajit@gmail.com>
2026-02-09 20:43:11 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    API_ID = 0  # Replace with your API ID (integer)
-    API_HASH = ""  # Replace with your API Hash
-    BOT_TOKEN = ""  # Replace with your Bot Token
+    API_ID = 25286584  # Replace with your API ID (integer)
+    API_HASH = "0b02bf8fc3250ad0b37bd339e29b1983"  # Replace with your API Hash
+    BOT_TOKEN = "8198028348:AAHCkggoS55DAP0fQ9wHQ1UXbUWkL8KE2Bw"  # Replace with your Bot Token
     MONGO_URI = "mongodb://localhost:27017"
     CHANNEL_ID = -1001234567890  # Replace with your Channel ID
-    ADMIN_IDS = [123456789]  # Replace with your Telegram User ID(s)
+    ADMIN_IDS = [7597291420]  # Replace with your Telegram User ID(s)
     CHECK_INTERVAL = 600
     CONCURRENCY_LIMIT = 3
     SITES_CONFIG = {"HDHub4u": True, "Bolly4u": True, "VegaMovies": True}
