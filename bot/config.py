37ab128e007efefe3f00ec27af21ea51b46dc13c
God Emperor <tarakindrajit@gmail.com>
2026-02-09 20:46:23 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = 25286584  # Replace with your API ID (integer)
     API_HASH = "0b02bf8fc3250ad0b37bd339e29b1983"  # Replace with your API Hash
     BOT_TOKEN = "8198028348:AAHCkggoS55DAP0fQ9wHQ1UXbUWkL8KE2Bw"  # Replace with your Bot Token
-    MONGO_URI = "mongodb://localhost:27017"
-    CHANNEL_ID = -1001234567890  # Replace with your Channel ID
+    MONGO_URI = "mongodb+srv://ltarak570_db_user:zAIRPn92DgjoVZsl@cluster0.bmrg5lf.mongodb.net/?appName=Cluster0"
+    CHANNEL_ID = -1002935398927  # Replace with your Channel ID
     ADMIN_IDS = [7597291420]  # Replace with your Telegram User ID(s)
     CHECK_INTERVAL = 600
     CONCURRENCY_LIMIT = 3
